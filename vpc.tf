8551d81588115ef2a982be5a1cba72efccf43c13
yubingjiaocn <yubingjiaocn@users.noreply.github.com>
2025-10-11 04:36:28 +0000

    Fix VPC endpoint issue

--- vpc.tf
+++ vpc.tf
@@ -4,6 +4,14 @@ data "aws_availability_zones" "available" {
 
 locals {
   azs = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  # VPC endpoint service name prefix varies by partition and endpoint type
+  # Gateway endpoints (S3, DynamoDB): com.amazonaws.{region}.{service} (same in all regions)
+  # Interface endpoints (STS, etc):
+  #   - Standard AWS: com.amazonaws.{region}.{service}
+  #   - China regions: cn.com.amazonaws.{region}.{service}
+  interface_endpoint_prefix = data.aws_partition.current.partition == "aws-cn" ? "cn.com.amazonaws" : "com.amazonaws"
+  gateway_endpoint_prefix   = "com.amazonaws"
 }
 
 module "vpc" {
@@ -39,7 +47,7 @@ module "vpc" {
 # VPC Endpoints for AWS services
 resource "aws_vpc_endpoint" "sts" {
   vpc_id             = module.vpc.vpc_id
-  service_name       = "com.amazonaws.${data.aws_region.current.region}.sts"
+  service_name       = "${local.interface_endpoint_prefix}.${data.aws_region.current.region}.sts"
   vpc_endpoint_type  = "Interface"
   subnet_ids         = module.vpc.private_subnets
   security_group_ids = [aws_security_group.vpc_endpoints.id]
@@ -49,7 +57,7 @@ resource "aws_vpc_endpoint" "sts" {
 
 resource "aws_vpc_endpoint" "s3" {
   vpc_id            = module.vpc.vpc_id
-  service_name      = "com.amazonaws.${data.aws_region.current.region}.s3"
+  service_name      = "${local.gateway_endpoint_prefix}.${data.aws_region.current.region}.s3"
   vpc_endpoint_type = "Gateway"
   route_table_ids   = module.vpc.private_route_table_ids
 }
